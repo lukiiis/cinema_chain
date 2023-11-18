bd9dbbee8188536ec6b055d0865c1222706cf8ad
lukiiis <lukaszczyszczon01@gmail.com>
2023-11-18 15:03:50 +0100

    przez ch

--- siec_kin_spring/src/main/java/com/ip/kino/controller/ZapowiedziController.java
+++ siec_kin_spring/src/main/java/com/ip/kino/controller/ZapowiedziController.java
@@ -10,6 +10,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1")
 public class ZapowiedziController {
+    int chuj;
     private final ZapowiedziService zapowiedziService;
     @Autowired
     public ZapowiedziController(ZapowiedziService zapowiedziService) {
