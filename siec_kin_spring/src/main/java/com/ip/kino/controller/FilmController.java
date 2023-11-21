1dac26862b0099db16def06327a0dc8165080510
zysio <krzyhsiem@gmail.com>
2023-11-22 00:07:41 +0100

    react i ustawienia

--- siec_kin_spring/src/main/java/com/ip/kino/controller/FilmController.java
+++ siec_kin_spring/src/main/java/com/ip/kino/controller/FilmController.java
@@ -3,6 +3,7 @@ package com.ip.kino.controller;
 import com.ip.kino.service.FilmService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
